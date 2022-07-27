748cbbb53ea4317738a05ba0c788ef5924e68fa5
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-07-27 23:06:58 +0530

    trial commit3

--- Main.py
+++ Main.py
@@ -99,4 +99,4 @@ evaluate = Evaluate(len(tokens))
 if evaluate.validate_postfix_expression(tokens):
     print(evaluate.evaluate_postfix_expression(tokens))
 else:
-    print('Invalid postfix expression')
+    print('Invalid postfix expression') 
